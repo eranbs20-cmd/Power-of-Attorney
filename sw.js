59ccf06bef91d4d76882ee15261e35bcb65c682b
eranbs20-cmd <eranbs20@gmail.com>
2026-05-21 00:08:20 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'poa-v11';
+const CACHE = 'poa-v12';
 const CORE = ['./poa.html','./base.pdf','./manifest.json','./icon-192.png','./icon-512.png'];
 const CDN = [
   'https://unpkg.com/pdf-lib/dist/pdf-lib.min.js',
